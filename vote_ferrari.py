19687d57b678573aab32182f1bae71e8e1484d7a
serendipitytech <troy@serendipitytech.net>
2024-02-18 16:05:41 -0500

    Update vote_ferrari.py

--- vote_ferrari.py
+++ vote_ferrari.py
@@ -34,7 +34,7 @@ def categorize_age(age):
     else:
         return "56+"
 
-@st.cache_data(allow_output_mutation=True)
+@st.cache_data
 def load_data():
     # Load data from S3 into a DataFrame
     try:
